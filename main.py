961901362c0b0423948bc9497250cf417c04dbc7
Sankalp22863 <naiksankalp1@gmail.com>
2022-03-06 12:12:00 +0530

    Imporved statup experience.

--- main.py
+++ main.py
@@ -86,15 +86,6 @@ st.title('Youtube Universe of Comments.')
 with st.expander("What is Youtbe universe of Comments??", expanded=True):
     st.markdown(welcome_txt)
 
-img_file_buffer = st.camera_input("Take a picture")
-
-if img_file_buffer is not None:
-    # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
-    # Check the type of bytes_data:
-    # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
-
 
 # Create a page dropdown
 # page = st.sidebar.selectbox("Choose your page", ["View Video."])
