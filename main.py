7d4f7731bbbdfc8fba65dc52a13de307821996bb
Sankalp22863 <naiksankalp1@gmail.com>
2022-03-06 11:01:10 +0530

    Changes in the flow

--- main.py
+++ main.py
@@ -47,56 +47,48 @@ import time
 import YoutubeAPI
 
 
-
 def vid():
-	global url 
-	global t
-	global Vids
-	global vid_search
-	global displayed
-	
-	if vid_search != '':
-		for i in range(10):
-			Vids.append(videosSearch.result()['result'][i]['title'])
-			url.append(videosSearch.result()['result'][i]['link'])
-		
-		for i in range(10):
-			t[Vids[i]] = url[i]
+    global url
+    global t
+    global Vids
+    global vid_search
+    global displayed
 
-		selected_video = st.selectbox('Select the Video', Vids)
+    if vid_search != '':
+        for i in range(10):
+            Vids.append(videosSearch.result()['result'][i]['title'])
+            url.append(videosSearch.result()['result'][i]['link'])
 
-		prev = selected_video
+        for i in range(10):
+            t[Vids[i]] = url[i]
 
-		url = t[selected_video]
-		st.subheader(selected_video)
+        selected_video = st.selectbox('Select the Video', Vids)
 
-		# Embed a youtube video
-		st_player(url)
-		
-		displayed = True
+        prev = selected_video
 
-	return 
+        url = t[selected_video]
+        st.subheader(selected_video)
 
+        # Embed a youtube video
+        st_player(url)
 
+        displayed = True
 
+    return
 
 
 # All the required Variables.
-
 url = []
 Vids = []
 t = {}
-
-
-
 
 
 # Starting with the Main App.
 
 st.title('Youtube Comment Analyzer.')
 
-# Create a page dropdown 
-# page = st.sidebar.selectbox("Choose your page", ["View Video."]) 
+# Create a page dropdown
+# page = st.sidebar.selectbox("Choose your page", ["View Video."])
 
 # st.sidebar.markdown("Clean up the Youtube Section.")
 # st.sidebar.markdown(" ")
@@ -107,97 +99,100 @@ st.title('Youtube Comment Analyzer.')
 # vid_search = st.text_input("Copy and paste the link of the URL of the youtube Video.")
 # videosSearch = VideosSearch(vid_search, limit = 10)
 
-st.selectbox("Choose which way you want to get the video :", ["Search the video online.", "Paste the URL of the video."])
+method = st.radio("Choose which way you want to get the video :", [
+                  "Search the video online.", "Search the video online."])
 
-url = st.text_input("Please enter the url of the video you want to check the comments of.")
-
-
-if url != "":	
-	youtube = YoutubeAPI.YoutubeAPI(url)
-	# parse video ID from URL.
-	youtube.video_id = youtube.get_video_id_by_url(url)
-	# make API call to get video info.
-	youtube.response = youtube.get_video_details(youtube.youtube, id=youtube.video_id)
-	# print extracted video infos.
-	disp_msg = st.subheader("Extracting the Information about the videos.")
-	youtube.get_video_infos(youtube.response)
-
-	# Displaying the Video information.
-	# print(f"""\
-        # Title: {title}
-        # Description: {description}
-        # Channel Title: {channel_title}
-        # Publish time: {publish_time}
-        # Duration: {duration_str}
-        # Number of comments: {comment_count}
-        # Number of likes: {like_count}
-        # Number of views: {view_count}
-        # """)
-
-	thumbnail_url = "http://img.youtube.com/vi/" + youtube.video_id +"/hqdefault.jpg"
-
-	c1, c2, c3 = st.columns(3)
-
-	c2.image(thumbnail_url)
-	
-	title = "Title : " + str(youtube.title)
-	channel = "Channel : " + str(youtube.channel_title)
-	likes = "Likes : " + youtube.like_count
-	views = "Views : " + youtube.view_count
-	comments = "Comments : " + youtube.comment_count
-
-	day, p_time = youtube.publish_time.split("T")
-	pub_time = "Video was Published on " + day + " at " + p_time[:-1]
-	hrs = 0
-	mins = 0
-	if "H" in youtube.duration:
-		hrs, min = youtube.duration[2:-1].split("H")
-		mins, secs = min.split("M")
-	elif "M" in youtube.duration:
-		mins, secs = youtube.duration[2:-1].split("M")
-	else:
-		secs = youtube.duration[2:-1]
-	if hrs != 0:
-		duration = "Video Duration : " + hrs + " Hours " + mins + " Mins and " + secs + " Secs."
-	elif mins != 0:
-		duration = "Video Duration : " + mins + " Mins and " + secs + " Secs."
-	else:
-		duration = "Video Duration : " + secs + " Secs."
-
-	st.subheader(title)
-	st.subheader(channel)
-	with st.expander("Description :"):
-		st.text(youtube.description)
+if method = "Search the video online.":
+    url = st.text_input(
+        "Please enter the url of the video you want to check the comments of.")
 
 
+if url != "":
+    youtube = YoutubeAPI.YoutubeAPI(url)
+    # parse video ID from URL.
+    youtube.video_id = youtube.get_video_id_by_url(url)
+    # make API call to get video info.
+    youtube.response = youtube.get_video_details(
+        youtube.youtube, id=youtube.video_id)
+    # print extracted video infos.
+    disp_msg = st.subheader("Extracting the Information about the videos.")
+    youtube.get_video_infos(youtube.response)
 
-	col1, col2, col3 = st.columns(3)
+    # Displaying the Video information.
+    # print(f"""\
+    # Title: {title}
+    # Description: {description}
+    # Channel Title: {channel_title}
+    # Publish time: {publish_time}
+    # Duration: {duration_str}
+    # Number of comments: {comment_count}
+    # Number of likes: {like_count}
+    # Number of views: {view_count}
+    # """)
 
-	col1.subheader(likes)
-	col2.subheader(views)
-	col3.subheader(comments)
+    thumbnail_url = "http://img.youtube.com/vi/" + \
+        youtube.video_id + "/hqdefault.jpg"
 
-	st.subheader(pub_time)
-	st.subheader(duration)
-	
+    c1, c2, c3 = st.columns(3)
 
-	disp_msg.subheader("Information Extracted Sucessfully. Now onto scrapping the Comments.")
-	youtube.scrap_comments(url)
-	# disp_msg.subheader("The Comments from the video extracted Sucessfully.")
-	disp_msg.text("")
+    c2.image(thumbnail_url)
 
-	youtube.df = youtube.df.drop("Comment_id", axis = 1)
+    title = "Title : " + str(youtube.title)
+    channel = "Channel : " + str(youtube.channel_title)
+    likes = "Likes : " + youtube.like_count
+    views = "Views : " + youtube.view_count
+    comments = "Comments : " + youtube.comment_count
 
-	for i in range(5):
-		name = youtube.df.iloc[i]
-		author_name = name["Author Name"]
-		comment_body = name["Comment"]
+    day, p_time = youtube.publish_time.split("T")
+    pub_time = "Video was Published on " + day + " at " + p_time[:-1]
+    hrs = 0
+    mins = 0
+    if "H" in youtube.duration:
+        hrs, min = youtube.duration[2:-1].split("H")
+        mins, secs = min.split("M")
+    elif "M" in youtube.duration:
+        mins, secs = youtube.duration[2:-1].split("M")
+    else:
+        secs = youtube.duration[2:-1]
+    if hrs != 0:
+        duration = "Video Duration : " + hrs + " Hours " + \
+            mins + " Mins and " + secs + " Secs."
+    elif mins != 0:
+        duration = "Video Duration : " + mins + " Mins and " + secs + " Secs."
+    else:
+        duration = "Video Duration : " + secs + " Secs."
+
+    st.subheader(title)
+    st.subheader(channel)
+    with st.expander("Description :"):
+        st.text(youtube.description)
+
+    col1, col2, col3 = st.columns(3)
+
+    col1.subheader(likes)
+    col2.subheader(views)
+    col3.subheader(comments)
+
+    st.subheader(pub_time)
+    st.subheader(duration)
+
+    disp_msg.subheader(
+        "Information Extracted Sucessfully. Now onto scrapping the Comments.")
+    youtube.scrap_comments(url)
+    # disp_msg.subheader("The Comments from the video extracted Sucessfully.")
+    disp_msg.text("")
+
+    youtube.df = youtube.df.drop("Comment_id", axis=1)
+
+    for i in range(5):
+        name = youtube.df.iloc[i]
+        author_name = name["Author Name"]
+        comment_body = name["Comment"]
 # 		c1, c2 = columns(2)
-		st.subheader(author_name)
-		st.text(comment_body)
+        st.subheader(author_name)
+        st.text(comment_body)
 
-	# st.dataframe(youtube.df[["Author Name", "Comment"]])
-	
+    # st.dataframe(youtube.df[["Author Name", "Comment"]])
+
 else:
-	st.markdown("Please Enter a Video URL to scrap the comments from.")
-
+    st.markdown("Please Enter a Video URL to scrap the comments from.")
