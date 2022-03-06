0d5a3ec3e81e415908d887ea47a47d62691250e8
Sankalp22863 <naiksankalp1@gmail.com>
2022-03-06 12:15:49 +0530

    Imporved statup experience.

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -14,7 +14,7 @@ import pickle
 class YoutubeAPI:
     def __init__(self, url):
         self.youtubeApiKey = "AIzaSyA9TQNt6htXodGXs_lX9mdWVEdmxHOh1do"
-        self.youtube = build('youtube', 'v3', developerKey = self.youtubeApiKey)
+        self.youtube = build('youtube', 'v3', developerKey=self.youtubeApiKey)
         # # parse video ID from URL
         # self.video_id = self.get_video_id_by_url(url)
         # # make API call to get video info
@@ -26,8 +26,8 @@ class YoutubeAPI:
         self.video_id = 0
         # make API call to get video info
         self.response = 0
-        return 
-    
+        return
+
     def get_video_id_by_url(self, url):
         """
         Return the Video ID from the video `url`
@@ -40,34 +40,38 @@ class YoutubeAPI:
             return video_id[0]
         else:
             raise Exception(f"Wasn't able to parse video URL: {url}")
-        
+
     def get_video_details(self, youtube, **kwargs):
         return youtube.videos().list(
             part="snippet,contentDetails,statistics",
             **kwargs
         ).execute()
-    
+
     def get_video_infos(self, video_response):
         items = video_response.get("items")[0]
         # get the snippet, statistics & content details from the video response
-        self.snippet         = items["snippet"]
-        self.statistics      = items["statistics"]
+        self.snippet = items["snippet"]
+        self.statistics = items["statistics"]
         self.content_details = items["contentDetails"]
         # get infos from the snippet
         self.channel_title = self.snippet["channelTitle"]
-        self.title         = self.snippet["title"]
-        self.description   = self.snippet["description"]
-        self.publish_time  = self.snippet["publishedAt"]
+        self.title = self.snippet["title"]
+        self.description = self.snippet["description"]
+        self.publish_time = self.snippet["publishedAt"]
         # get stats infos
-        self.comment_count = self.statistics["commentCount"]
-        self.like_count    = self.statistics["likeCount"]
+        try:
+            self.comment_count = self.statistics["commentCount"]
+        except KeyError:
+            self.comment_count = 0
+        self.like_count = self.statistics["likeCount"]
         # dislike_count = statistics["dislikeCount"]
-        self.view_count    = self.statistics["viewCount"]
+        self.view_count = self.statistics["viewCount"]
         # get duration from content details
         self.duration = self.content_details["duration"]
         # duration in the form of something like 'PT5H50M15S'
         # parsing it to be something like '5:50:15'
-        self.parsed_duration = re.search(f"PT(\d+H)?(\d+M)?(\d+S)", self.duration).groups()
+        self.parsed_duration = re.search(
+            f"PT(\d+H)?(\d+M)?(\d+S)", self.duration).groups()
         self.duration_str = ""
         for d in self.parsed_duration:
             if d:
@@ -83,32 +87,32 @@ class YoutubeAPI:
         # Number of likes: {like_count}
         # Number of views: {view_count}
         # """)
-        return 
+        return
 
     def get_comments(self, youtube, **kwargs):
         return youtube.commentThreads().list(
             part="snippet",
             **kwargs
         ).execute()
-    
+
     def scrap_comments(self, url):
         if "watch" in url:
             # that's a video
             video_id = self.get_video_id_by_url(url)
             params = {
-                'videoId': video_id, 
+                'videoId': video_id,
                 # 'maxResults': 1,
-                'order': 'relevance', # default is 'time' (newest)
+                'order': 'relevance',  # default is 'time' (newest)
             }
         else:
             # should be a channel
             channel_id = self.get_channel_id_by_url(url)
             params = {
-                'allThreadsRelatedToChannelId': channel_id, 
+                'allThreadsRelatedToChannelId': channel_id,
                 # 'maxResults': 2,
-                'order': 'relevance', # default is 'time' (newest)
+                'order': 'relevance',  # default is 'time' (newest)
             }
-        
+
         done = False
         comments = 0
         self.df = []
@@ -136,12 +140,13 @@ class YoutubeAPI:
             if "nextPageToken" in response:
                 # if there is a next page
                 # add next page token to the params we pass to the function
-                params["pageToken"] =  response["nextPageToken"]
+                params["pageToken"] = response["nextPageToken"]
             else:
                 # must be end of comments!!!!
                 done = True
                 break
-        
-        self.df = pd.DataFrame(self.df, columns = ['Comment_id','Author Name' ,'Comment', 'Updated_at', 'Like_Count' ])
-        
+
+        self.df = pd.DataFrame(self.df, columns=[
+                               'Comment_id', 'Author Name', 'Comment', 'Updated_at', 'Like_Count'])
+
         return page_no, comments
