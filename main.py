8e21f90564a9413da8eb810f9722d1a45b7fdf7a
Sankalp22863 <naiksankalp1@gmail.com>
2022-03-06 11:57:28 +0530

    Imporved statup experience.

--- main.py
+++ main.py
@@ -67,14 +67,12 @@ t = {}
 
 st.title('Youtube Universe of Comments.')
 
-welcome_txt = '''Years for now the comment section of Youtube has been plagued with 
-                random spam content and the youtube doesn't seem to doing anything about 
-                   it. We here introduce a new comment section revamped to make the good comments 
-                       float to the top and the spam ones to linger to the bottom.'''
+welcome_txt = '''Years for now the comment section of Youtube has been plagued with random spam content and the youtube doesn't seem to doing anything about it. We here introduce a new comment section revamped to make the good comments float to the top and the spam ones to linger to the bottom.'''
 
-st.title('Welcome to Youtube Universe of Comments.')
 
 st.text(welcome_txt)
+
+st.title('Welcome to Youtube Universe of Comments.')
 
 # Create a page dropdown
 # page = st.sidebar.selectbox("Choose your page", ["View Video."])
