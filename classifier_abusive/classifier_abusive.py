331790dd4bdb132f9e0ba22e5b3a1bb365219664
asxd-10 <katreashay@gmail.com>
2022-04-03 23:32:58 +0530

    classifier self modify

--- classifier_abusive/classifier_abusive.py
+++ classifier_abusive/classifier_abusive.py
@@ -13,7 +13,7 @@ class classifier_abusive:
         self.labelledText = pd.read_csv(labelled_data)
 
 
-    def customized_preprocess(inp_text):
+    def customized_preprocess(self, inp_text):
         url_detect = '/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm'
         mentiontag_regex = '@[\w\-]+'
         hashtag_regex = '#[\w\-]+'
@@ -24,29 +24,29 @@ class classifier_abusive:
         return parsed_text
 
 
-    def tokenization(inp_text):
+    def tokenization(self, inp_text):
         return (" ".join(re.split("[^a-zA-Z]*", inp_text.lower())).strip())
     
-    def lemmatization(inp_text):
+    def lemmatization(self, inp_text):
         stemmers = [PorterStemmer(),SnowballStemmer(),LancasterStemmer(),RegexpStemmer()]
         stemmer = stemmers[0]
         return [stemmer.stem(t) for t in inp_text.split()]
     
-    def get_pos_tags(comments):
+    def get_pos_tags(self, comments):
         comment_tags = []
         for c in comments:
-            tokens = tokenization(customized_preprocess(c))
+            tokens = self.tokenization(self.customized_preprocess(c))
             tags = nltk.pos_tag(tokens)
             tag_list = [x[1] for x in tags]
             tag_str = " ".join(tag_list)
             comment_tags.append(tag_str)
         return comment_tags
 
-    def other_features_(comment):
+    def other_features_(self, comment):
         sentiment_analyzer = SIA()
         sentiment = sentiment_analyzer.polarity_scores(comment)
 
-        words = customized_preprocess(comment) #Get text only
+        words = self.customized_preprocess(comment) #Get text only
 
         syllables = textstat.syllable_count(words) #count syllables in words
         num_chars = sum(len(w) for w in words) #num chars in words
@@ -66,34 +66,34 @@ class classifier_abusive:
         #features = pandas.DataFrame(features)
         return features
 
-    def get_oth_features(tweets):
+    def get_oth_features(self, tweets):
         """Takes a list of tweets, generates features for
         each tweet, and returns a numpy array of tweet x features"""
         feats=[]
         for t in tweets:
-            feats.append(other_features_(t))
+            feats.append(self.other_features_(t))
         return np.array(feats)
 
-    def transformation(inp_comments, tf_vectorizer, idf_vector, pos_vectorizer):
+    def transformation(self, inp_comments, tf_vectorizer, idf_vector, pos_vectorizer):
         tf_array = tf_vectorizer.fit_transform(inp_comments).toarray()
         tfidf_array = tf_array*idf_vector
         print("Built TF-IDF array")
 
-        pos_tags = get_pos_tags(inp_comments)
+        pos_tags = self.get_pos_tags(inp_comments)
         pos_array = pos_vectorizer.fit_transform(pos_tags).toarray()
         print("Built POS array")
 
-        oth_array = get_oth_features(inp_comments)
+        oth_array = self.get_oth_features(inp_comments)
         print("Built other feature array")
 
         M = np.concatenate([tfidf_array, pos_array, oth_array],axis=1)
         return pd.DataFrame(M)
 
-    def predictions(X, model):
+    def predictions(self, X, model):
         y_preds = model.predict(X)
         return y_preds
 
-    def class_to_name(class_label):
+    def class_to_name(self, class_label):
         if class_label == 0:
             return "Hate speech"
         elif class_label == 1:
@@ -104,7 +104,7 @@ class classifier_abusive:
             return "No class"
 
     
-    def classify_comments(comments):
+    def classify_comments(self, comments):
         fixed_comments = []
         for i, t_orig in enumerate(comments):
             s = t_orig
@@ -135,29 +135,29 @@ class classifier_abusive:
         #Load function to transform data
 
         print("Transforming inputs...")
-        X = transformation(comments, tf_vectorizer, idf_vector, pos_vectorizer)
+        X = self.transformation(comments, tf_vectorizer, idf_vector, pos_vectorizer)
 
         print("Running classification model...")
-        predicted_class = predictions(X, model)
+        predicted_class = self.predictions(X, model)
 
         return predicted_class
 
     def classify(self):
         inp_comments = self.text.Text
         inp_comments = [x for x in inp_comments if type(x) == str]
-        train_preds = classify_comments(inp_comments)
+        train_preds = self.classify_comments(inp_comments)
 
         print ("Printing predicted values: ")
         for i,c in enumerate(inp_comments):
             print(c)
-            print (class_to_name(train_preds[i]))
+            print (self.class_to_name(train_preds[i]))
 
         print("Calculate accuracy on labeled data")
         df = pd.read_csv('../data/labeled_data.csv')
         test_comments = self.labelledText.values
         test_comments = [x for x in test_comments if type(x) == str]
         comments_class = test_comments['class'].values
-        test_preds = classify_comments(test_comments)
+        test_preds = self.classify_comments(test_comments)
         right_count = 0
         for i,t in enumerate(test_comments):
             if comments_class[i] == test_preds[i]:
